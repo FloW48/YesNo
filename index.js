c101a22c53d73576190840debbf667e3d31798b7
Ubuntu <ubuntu@vps-33f203ea.vps.ovh.net>
2020-10-27 20:21:18 +0000

    v1 push

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ bot.on('ready', function(){
 })
 
 bot.on('message', function (message){
-    if(botOn && !pauseBot && message.channel.id == "666563454517248006"){
+    if(botOn && !pauseBot){
         checkMessage(message)
     }
-    changeState(message)
+    commands(message)
 })
 
 bot.login('NzYxNTA4NTQyOTUxMTk0NjQ2.X3boGA.cjJic_A6XM0R7GlJ2FnTbSrHKYg')
@@ -24,16 +24,33 @@ bot.login('NzYxNTA4NTQyOTUxMTk0NjQ2.X3boGA.cjJic_A6XM0R7GlJ2FnTbSrHKYg')
 function checkMessage(message){
     message.content = message.content.toLowerCase()
     const args = message.content.split(/[ ,.:_*/;?!]/);
-    console.log(args)
-    if(message.author.id == "256054054260572161"){
-        if(message.content.startsWith("nion add ")){
-            bannedWords.push(args[2])
-        }
-        else if(message.content.startsWith("nion remove ")){
-            bannedWords.splice(bannedWords.indexOf(args[2]), 1);
+    if(message.channel.id == "666563454517248006"){
+        for(i = 0; i < args.length; i++){
+            if(bannedWords.indexOf(args[i]) != -1){
+                goulag(message)
+            }
         }
     }
-    if(message.content.startsWith("nion show")){
+}
+
+async function goulag(message){
+    let member = await message.member
+    let role = await message.guild.roles.cache.find(r => r.name == 'Goulag');
+    console.log(role)
+    member.roles.add(role)
+    setTimeout(function() {
+        member.roles.remove(role)
+    }, 10000)
+}
+
+function commands(message){
+    message.content = message.content.toLowerCase()
+    const args = message.content.split(/[ ,.:_*/;?!]/);
+    if(message.content.startsWith("nion state")){
+        let isOn =  botOn ? 'on' : 'off'
+        message.channel.send("Bot is **"+isOn+"**")
+    }
+    else if(message.content.startsWith("nion show")){
         pauseBot = true;
         let stringBuilder = ""
         for(i = 0; i < bannedWords.length; i++){
@@ -42,28 +59,21 @@ function checkMessage(message){
         message.channel.send("Mots bannis : "+stringBuilder)
         pauseBot = false;
     }
-    else{
-        for(i = 0; i < args.length; i++){
-            if(bannedWords.indexOf(args[i]) != -1){
-                member = message.member
-                let role = message.guild.roles.cache.find(r => r.name === "Combatant");
-                goulag(member, role)
-            }
-        }
-    }
-}
+    else if(message.content.startsWith("nion help")){
+        const embed = new Discord.MessageEmbed();
+        embed.setTitle("Ni oui ni non")
+        embed.setAuthor(bot.user.username,  bot.user.avatarURL())
+        embed.setTimestamp(Date.now());
+        embed.setColor([240,176,255])
 
-function goulag(member, role){
-    member.roles.add(role)
-    setTimeout(function() {
-        member.roles.remove(role)
-    }, 10000)
-}
+        embed.addField("Commandes pour tout le monde", 
+        ">>> • nion state\n"+
+        " • nion show\n")
 
-function changeState(message){
-    if(message.content.startsWith("nion state")){
-        let isOn =  botOn ? 'on' : 'off'
-        message.channel.send("Bot is **"+ isOn+"**")
+        embed.addField("Commandes pour admin", 
+        ">>> • nion on/off\n"+
+        " • nion ban/unban `word`")
+        message.channel.send(embed);
     }
     if(  (message.member != null && message.member.hasPermission("ADMINISTRATOR")) || message.author.id == 256054054260572161 ){
         if(message.content.startsWith("nion on")){
@@ -77,6 +87,14 @@ function changeState(message){
             botOn = false
             let isOn =  botOn ? 'on' : 'off'
             bot.user.setActivity("Bot "+isOn).catch(console.error);
+        }
+        else if(message.content.startsWith("nion ban ")){
+            bannedWords.push(args[2])
+            message.channel.send(args[2]+" est désormais un mot banni")
+        }
+        else if(message.content.startsWith("nion unban ")){
+            bannedWords.splice(bannedWords.indexOf(args[2]), 1);
+            message.channel.send(args[2]+" n'est désormais plus un mot banni")
         }
     }
 }
